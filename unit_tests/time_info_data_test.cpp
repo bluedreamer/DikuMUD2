a56a4920c04241ec9b77f33a5358fd92b8b15679
Adrian Cornish <github@bluedreamer.com>
2021-12-20 10:06:04 -0700

    More time_info_data tests

--- unit_tests/time_info_data_test.cpp
+++ unit_tests/time_info_data_test.cpp
@@ -1,7 +1,74 @@
+#include "../dm-dist-ii/constants.h"
 #include "../dm-dist-ii/structs.h"
 #include "../dm-dist-ii/weather.h"
 #include <boost/test/unit_test.hpp>
+#include <vector>
 
+struct Fixture
+{
+   Fixture()
+   {
+      populate_days();
+      populate_months();
+   }
+   void populate_months()
+   {
+      months.push_back("Month of Winter");
+      months.push_back("Month of the Winter Wolf");
+      months.push_back("Month of the Frost Giant");
+      months.push_back("Month of the Spring");
+      months.push_back("Month of Futility");
+      months.push_back("Month of the Sun");
+      months.push_back("Month of the Heat");
+      months.push_back("Month of the Long Shadows");
+      months.push_back("Month of the Ancient Darkness");
+   }
+
+   void populate_days()
+   {
+      days.push_back("the Day of the Moon");
+      days.push_back("the Day of the Bull");
+      days.push_back("the Day of the Deception");
+      days.push_back("the Day of Thunder");
+      days.push_back("the Day of Freedom");
+      days.push_back("the Day of the Great Gods");
+      days.push_back("the Day of the Sun");
+   }
+   auto day_prefix(int day) -> std::string
+   {
+      if(day == 1)
+      {
+         return "st";
+      }
+      else if(day == 2)
+      {
+         return "nd";
+      }
+      else if(day == 3)
+      {
+         return "rd";
+      }
+      else if(day < 20)
+      {
+         return "th";
+      }
+      else if((day % 10) == 1)
+      {
+         return "st";
+      }
+      else if((day % 10) == 2)
+      {
+         return "nd";
+      }
+      else if((day % 10) == 3)
+      {
+         return "rd";
+      }
+      return "th";
+   }
+   std::vector<std::string> days;
+   std::vector<std::string> months;
+};
 BOOST_AUTO_TEST_CASE(time_info_data_defines)
 {
    BOOST_TEST(SECS_PER_REAL_MIN == 60);
@@ -18,6 +85,17 @@ BOOST_AUTO_TEST_CASE(time_info_data_defines)
    BOOST_TEST(SECS_PER_MUD_DAY == 7200);
    BOOST_TEST(SECS_PER_MUD_MONTH == 100800);
    BOOST_TEST(SECS_PER_MUD_YEAR == 907200);
+
+   BOOST_TEST(beginning_of_time == 650336715);
+}
+
+BOOST_AUTO_TEST_CASE(time_info_data_constants_tests)
+{
+   Fixture f;
+   for(auto i = 0; i < MUD_WEEK; ++i)
+   {
+      BOOST_TEST(std::string(weekdays[i]) == f.days[i]);
+   }
 }
 
 BOOST_AUTO_TEST_CASE(real_time_passed_test)
@@ -152,5 +230,92 @@ BOOST_AUTO_TEST_CASE(mud_time_passed_test)
       BOOST_TEST(result.day == 12);
       BOOST_TEST(result.month == 6);
       BOOST_TEST(result.year == 34);
+   }
+}
+
+BOOST_AUTO_TEST_CASE(mud_date_test)
+{
+   time_t         now    = 1640014705; // Mon Dec 20 08:38:25 2021
+   time_info_data result = mud_date(now);
+   BOOST_TEST(result.hours == 6);
+   BOOST_TEST(result.day == 3);
+   BOOST_TEST(result.month == 8);
+   BOOST_TEST(result.year == 1090);
+}
+
+BOOST_AUTO_TEST_CASE(mudtime_strcpy_test)
+{
+   {
+      // Test different hours
+      const std::string msg{", on the Day of the Bull,\n\r   the 1st Day of the Month of Winter, Year 0.\n\r"};
+
+      time_info_data time{};
+      for(time.hours = -24; time.hours <= 24; ++time.hours)
+      {
+         time.day   = 0;
+         time.month = 0;
+         time.year  = 0;
+         char buffer[4096]{};
+         mudtime_strcpy(&time, buffer);
+         std::ostringstream strm;
+         int                display_hour = ((time.hours % 12 == 0) ? 12 : (time.hours % 12));
+         strm << display_hour << " o'clock ";
+         if((time.hours > 12 || time.hours == 0))
+         {
+            strm << "after Noon";
+         }
+         else
+         {
+            strm << "past Midnight";
+         }
+         strm << msg;
+         BOOST_TEST(std::string(buffer) == strm.str());
+      }
+   }
+   ///////////////////////////////////////////////////////////////////////////
+   {
+      // Test different days
+      Fixture           f;
+      const std::string start_msg{"12 o'clock after Noon, on "};
+      const std::string mid_msg{",\n\r   the "};
+      const std::string end_msg{" Day of the Month of Winter, Year 0.\n\r"};
+      time_info_data    time{};
+      for(time.day = 0; time.day < MUD_WEEK; ++time.day)
+      {
+         time.hours = 0;
+         time.month = 0;
+         time.year  = 0;
+         char buffer[4096]{};
+         mudtime_strcpy(&time, buffer);
+
+         std::ostringstream strm;
+         int                real_day = time.day + 1;
+         int                week_day = ((MUD_MONTH * time.month) + time.day + 1) % MUD_WEEK;
+         strm << start_msg << f.days[week_day] << mid_msg << real_day << f.day_prefix(real_day) << end_msg;
+         BOOST_TEST(std::string(buffer) == strm.str());
+      }
+   }
+   ///////////////////////////////////////////////////////////////////////////
+   {
+      // Test different months
+      Fixture           f;
+      const std::string start_msg{"12 o'clock after Noon, on "};
+      const std::string mid_msg{",\n\r   the "};
+      time_info_data    time{};
+      for(time.month = 0; time.month < MUD_YEAR; ++time.month)
+      {
+         time.day   = 0;
+         time.hours = 0;
+         time.year  = 0;
+         char buffer[4096]{};
+         mudtime_strcpy(&time, buffer);
+
+         std::ostringstream strm;
+         int                real_day = time.day + 1;
+         int                week_day = ((MUD_MONTH * time.month) + time.day + 1) % MUD_WEEK;
+         strm << start_msg << f.days[week_day] << mid_msg << real_day << f.day_prefix(real_day) << " Day of the " << f.months[time.month]
+              << ", Year 0.\n\r";
+         BOOST_TEST(std::string(buffer) == strm.str());
+      }
    }
 }
