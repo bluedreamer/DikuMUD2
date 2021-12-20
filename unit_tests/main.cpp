fe8629001723a3d180aec1266ec4b2ab165c3b2b
Adrian Cornish <github@bluedreamer.com>
2021-12-20 07:22:42 -0700

    Toplevel include_directories breaks boost test for some reason

---
+++ unit_tests/main.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE "Diku MUD 2 Tests"
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+//BOOST_AUTO_TEST_CASE(LineTest)
+//{
+//   BOOST_CHECK(5 == 6);
+//}
